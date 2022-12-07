a56eefbda570207ad4772d6c6edd3660ac6c3fc9
Trinity <amoahtnt6@gmail.com>
2022-12-07 00:02:33 +0000

    Step two done left tog

--- src/components/mobile/OptionCard.jsx
+++ src/components/mobile/OptionCard.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const OptionCard = ({ img, title, price, isSelected,onClick }) => {
     return (
         <div onClick={onClick} className="option-card" style={{
-            border: isSelected ? '1px solid var(--PurplishBlue)' : '1px solid var(--LightGray)'
+            border: isSelected ? '1px solid var(--PurplishBlue)' : '1px solid var(--LightGray)',
+            backgroundColor: isSelected ? 'var(--Magnolia)': null
         }}>
             <img src={img} alt="Arcade" />
             <div className="mini-info">
